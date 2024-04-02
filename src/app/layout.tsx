cfd12aa08d5e915b5634bdb16dc403f2fd78d869
axel <axosar@gmail.com>
2024-04-02 13:59:17 -0600

    test-X

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "APITECHA",
-  description: "Yes",
+  description: "Desarrollo de aplicaciones freelance",
 };
 
 export default function RootLayout({
