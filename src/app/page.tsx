4bac45c5f200d6627912d2530116fcff68b38db9
axel <axosar@gmail.com>
2024-04-02 13:38:29 -0600

    test-E

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            APITECHSA
+            APITEChA
           </p>
         </a>
 
