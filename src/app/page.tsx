dbbd6011fae8a1ccec0e4fb5e9c7e98328534f5a
axel <axosar@gmail.com>
2024-04-02 19:19:56 -0600

    test-CHALE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import Image from "next/image";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between w-100">
       <Header/>
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+      <Footer/>
     </main>
   );
 }
