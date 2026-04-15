b9012bb187a96f519a876d92402a0fcddbf0a8a7
Axel Obscura <axosar@gmail.com>
2026-04-15 03:18:22 +0000

    footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,22 +11,22 @@ export default function Footer() {
       <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
           <a href="/somos" className="hover:underline me-4 md:me-6">
-            About
+            Somos
           </a>
         </li>
         <li>
           <a href="#" className="hover:underline me-4 md:me-6">
-            Privacy Policy
+            Política de Privacidad
           </a>
         </li>
         <li>
           <a href="#" className="hover:underline me-4 md:me-6">
-            Licensing
+            Licencia
           </a>
         </li>
         <li>
           <a href="#" className="hover:underline">
-            Contact
+            Contacto
           </a>
         </li>
       </ul>
